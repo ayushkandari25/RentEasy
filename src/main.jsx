65be3166d3a45c67b6bd560f9fcd46302098a359
ayushkandari25 <ayush25.kandari@gmail.com>
2025-03-22 20:19:40 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext.jsx";
-// import { ThemeProvider } from "./Context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    {/* <ThemeProvider> */}
       <AuthProvider>
         <App />
       </AuthProvider>
-    {/* </ThemeProvider> */}
   </BrowserRouter>
 );
